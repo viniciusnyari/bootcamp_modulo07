6ca6861b9b82ba81b95adde0156dc64f6b99dfb2
ViniciusNyari <viniciusnyari@gmail.com>
2020-06-03 10:57:16 -0300

    Criando Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+import logo from '../../assets/images/logo.svg';
 
 function Header() {
   return (
     <Container>
-      <img src={} alt="Rocketshoes"/>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
     </Container>
   );
 }
