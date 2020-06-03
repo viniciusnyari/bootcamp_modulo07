3808c59c8f308a34cb76e320d28ee257b40b9634
ViniciusNyari <viniciusnyari@gmail.com>
2020-06-03 08:52:00 -0300

    Criando interface

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <img src={} alt="Rocketshoes"/>
+    </Container>
+  );
+}
+
+export default Header;
